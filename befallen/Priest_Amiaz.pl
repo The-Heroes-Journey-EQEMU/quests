a5a295b79bba028c9cb2dd7601aa426ed9c7956e
Trust <fryguy503@gmail.com>
2025-01-20 00:00:03 -0500

    [Cleanup] Misc Befallen Cleanup

--- befallen/Priest_Amiaz.pl
+++
@@ -1,8 +0,0 @@
-sub EVENT_DEATH_COMPLETE {
-	quest::say("You may have won this battle, my friend, but I shall rise again... and one day, all shall know the true power of the Burning Dead!");
-}
-
-sub EVENT_ITEM {
-	#:: Return unused items
-	plugin::return_items(\%itemcount);
-}
