f316207862946416a464ae07bea7de5515e85435
Trust <fryguy503@gmail.com>
2025-01-20 19:28:23 -0500

    [Cleanup] Shaman Cudgel/Skull Quest
    - Added Flags to each step to monitor progress
    - Added faction and level checking per step
    - Added notice of flags being assigned (So they are not "Invisible")
    - Added Dialog to Oxyn "Progress" that will output current progress point.
    
    Also limited a quest turn in that can be exploited for fast and unlimited XP.

--- burningwood/Atheling_Plague.lua
+++ burningwood/Atheling_Plague.lua
@@ -1,29 +1,34 @@
---Shaman Skull Quest 6
--- items: 12764
-
-
+--Shaman Cudgel Quest 6
 function event_say(e)
-	if e.other:HasItem(5145) then
-		if e.message:findi("hail") then
-			e.self:Say("Ahh!! A conversationalist. How good to meet you, " .. e.other:GetCleanName() .. ". Yes. I have heard of you. Go ahead and ask for that which has brought you to my tower and emboldened you to slay my weaker minions.");
-		elseif e.message:findi("sisters of scale") then
-			e.self:Say("What a coincidence! I, too, seek a skull. Perhaps you might help me [obtain the skull]. Perhaps then you shall have the skull you desire.");
-		elseif e.message:findi("obtain the skull") then
-			e.self:Say("I am sure you would not mind removing the head of a scaled mystic. I hope not. There is an old Iksar who once called me slave. Now he shall adorn my wall, mounted on a fine plaque. His name is Digalis. Find him. Do not return until your task is complete.");
+	local Shaman_Cudgel_Progress	= e.other:GetBucket("Shaman_Cudgel")
+
+	if Shaman_Cudgel_Progress == "6.1" then
+		if e.other:GetLevel() >= 45 then
+			if e.message:findi("hail") then
+				e.self:Say("Ahh!! A conversationalist. How good to meet you, " .. e.other:GetCleanName() .. ". Yes. I have heard of you. Go ahead and ask for that which has brought you to my tower and emboldened you to slay my weaker minions.");
+			elseif e.message:findi("sisters of scale") then
+				e.self:Say("What a coincidence! I, too, seek a skull. Perhaps you might help me [obtain the skull]. Perhaps then you shall have the skull you desire.");
+			elseif e.message:findi("obtain the skull") then
+				e.self:Say("I am sure you would not mind removing the head of a scaled mystic. I hope not. There is an old Iksar who once called me slave. Now he shall adorn my wall, mounted on a fine plaque. His name is Digalis. Find him. Do not return until your task is complete.");
+			end
+		else
+			e.self:Say("Come back when you are more experienced and you can assist us.");
 		end
+	else
+		e.self:Say("Go away, I am busy.");
 	end
 end
 
 function event_trade(e)
-	local item_lib = require("items");
+	local item_lib					= require("items");
+	local Shaman_Cudgel_Progress	= e.other:GetBucket("Shaman_Cudgel")
 
-	if e.other:HasItem(5145) then
-		if item_lib.check_turn_in(e.trade, {item1 = 12764}) then
-			e.other:QuestReward(e.self,0,0,0,0,12750);
-			e.self:Shout("Excellent. You show signs of a true Iksar slayer. Too, bad I have already given the skull of the Sister of Scale to another. Perhaps you would like to meet him before he departs. Say hello, Doval.");
-			eq.unique_spawn(87154,0,0,-4067,6351,-53, 0);
-			eq.set_timer("heal",20 * 1000);
-		end
+	if Shaman_Cudgel_Progress == "6.1" and item_lib.check_turn_in(e.trade, {item1 = 12764}) then -- Item: Iksar Skull
+		e.self:Shout("Excellent. You show signs of a true Iksar slayer. Too, bad I have already given the skull of the Sister of Scale to another. Perhaps you would like to meet him before he departs. Say hello, Doval.");
+		e.other:SummonItem(12750);						-- Item: Iksar Skull
+		e.other:QuestReward(e.self,{exp = 50});
+		eq.unique_spawn(87154,0,0,-4067,6351,-53, 0);	-- NPC: Clerk_Doval
+		eq.set_timer("heal",20 * 1000);
 	end
 	item_lib.return_items(e.self, e.other, e.trade);
 end
--- charasis/Venril_Sathir.pl
+++
@@ -1,9 +0,0 @@
-sub EVENT_DEATH_COMPLETE {
-    quest::unique_spawn(105187,0,0,$x,$y,$z); # NPC: the_spirit_of_Rile
-
-    my $killer = $entity_list->GetClientByID($killer_id);   
-
-    if ($killer && int(rand(100)) == 0) {
-        plugin::AddTitleFlag(202);
-    }
-}
