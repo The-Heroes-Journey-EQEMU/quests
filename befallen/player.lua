05c16b617a421a3cfcb32439c6af5315a701e231
Fryguy <fryguy503@gmail.com>
2025-01-24 13:45:49 -0500

    [Cleanup] Misc Befallen Cleanup (#120)

--- befallen/player.lua
+++ befallen/player.lua
@@ -1,12 +1,8 @@
 function event_enter_zone(e)
-	if(e.self:HasClass(Class.ROGUE) or e.self:HasClass(Class.BARD)) then
-		if(e.self:HasItem( 52355) == true and e.self:HasItem( 52357) == false) then --Shakey's Dilapidated Noggin, Pulsing Rose Lightstone
-			eq.unique_spawn(36102, 2, 0, 165, -170, -10); --Flighty_Rose_Wisp
-			e.self:Message(15, "Out of the corner of your eye, you catch a glimpse of a brightly colored wisp as it darts by. It's moving at an incredible speed.");
+	if e.self:HasClass(Class.ROGUE) or e.self:HasClass(Class.BARD) then
+		if e.self:HasItem(52355)and not e.self:HasItem(52357) then -- Items: (Has) Shakey's Dilapidated Noggin, (Does Not Have) Pulsing Rose Lightstone
+			eq.unique_spawn(36102, 2, 0, 165, -170, -10); -- NPC: Flighty_Rose_Wisp
+			e.self:Message(MT.Yellow, "Out of the corner of your eye, you catch a glimpse of a brightly colored wisp as it darts by. It's moving at an incredible speed.");
 		end
 	end
 end
--------------------------------------------------------------------------------------------------
--- Converted to .lua using MATLAB converter written by Stryd
--- Find/replace data for .pl --> .lua conversions provided by Speedz, Stryd, Sorvani and Robregen
--------------------------------------------------------------------------------------------------
