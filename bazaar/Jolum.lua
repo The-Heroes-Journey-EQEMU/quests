10cb6da46f6f7cd5ad18bb7b929e50354db71df7
Catapultam <alex@catapult.am>
2025-02-01 15:28:47 -0600

    jolum revisions

--- bazaar/Jolum.lua
+++ bazaar/Jolum.lua
@@ -1,10 +1,7 @@
 -- items: 17900, 17138
 function event_say(e)
 	if(e.message:findi("hail")) then
-		e.self:Say("Hail! Nice to see ya, friend. I've got a fine selection of spankin' good spells, perhaps you'd be interested? Or maybe you'd like to learn how to [upgrade] your Grandmaster Trade items? Still yet, maybe you would like to know how to create a [Grandmaster's Medicine Bag]?");
-
-	elseif(e.message:findi("upgrade")) then
-		e.self:Say("What Grandmaster Trade item would you like to know how to upgrade? I can tell you how to upgrade your [Grandmaster Baker's Spoon], [Grandmaster Brewer's Corker], [Grandmaster Fletcher's Knife], [Grandmaster Jeweler's Eyeglass], [Grandmaster Potter's Sculpter], [Grandmaster Smith's Hammer], [Grandmaster Tailor's Needle], or your [Grandmaster Tinker's Spanner]. While you can't have more than one of the new items, they will summon a [bag] for you. Very useful for carrying heavy items about and such.");
+		e.self:Say("Hail! Nice to see ya, friend. I've got a fine selection of spankin' good spells, perhaps you'd be interested? Or maybe you'd like to learn how to make the [bag] from your Grandmaster Trade items a little more permanent?");
 
 	elseif(e.message:findi("bag")) then
 		e.self:Say("Its a very sturdy bag, but I'm afraid that it's only temporary. If you'd like to get a more permanent bag, just hand me your summoned one and I'll trade it out you.");
