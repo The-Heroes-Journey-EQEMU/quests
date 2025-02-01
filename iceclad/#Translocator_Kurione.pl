9d3168a1e274afbf51176d4fdd9032c36361748c
RetributionEQ <120991589+RetributionEQ@users.noreply.github.com>
2025-02-01 11:47:20 -0500

    Fix TL Kurione probably

--- iceclad/#Translocator_Kurione.pl
+++ iceclad/#Translocator_Kurione.pl
@@ -4,10 +4,10 @@ sub EVENT_SAY {
     quest::say("Hello there. There seems to be some strange problems with the boats in this area. The Academy of Arcane Sciences has sent a small team of us to investigate them. If you need to [" . quest::saylink("travel to North Ro") . "] in the meantime, I can transport you to my companion there. We also just recently discovered that Joshel has been stranded over in the middle of the ocean since the problems with the boats started. If you'd be willing to go see if he's ok, I may be able to [" . quest::saylink("teleport you near there") . "]. Keep in mind though that it will be a one way trip. There is no one on the island able to send you back.");
   }
   if ($text=~/teleport near there/i) {
-    quest::movepc($zoneid,-20266.8,3985.5,0); # player is moved near Joshel
+    $client->MovePC(110, -20288, 3886, -7, 396); # Near Joshel
   }
   if ($text=~/travel to north ro/i) {
-    quest::selfcast("2282");
+    $client->MovePC(34, 304, 2664, -25, 241); # Old Nro
   }
 }
 
