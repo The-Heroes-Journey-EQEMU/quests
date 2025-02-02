c801e61934c4bfad918c4b36ad895610b335f9d3
Pippz <piattnd@gmail.com>
2025-02-02 14:36:00 -0700

    Fixing comment on badain spawn condition

--- greatdivide/Sentry_Badain.lua
+++ greatdivide/Sentry_Badain.lua
@@ -1,7 +1,7 @@
 local war_started = false
 
 function event_spawn(e)
-	if (tostring(eq.get_zone_instance_version()) != eq.get_rule("Custom:StaticInstanceVersion")) then -- Don't spawn Badain in open world
+	if (tostring(eq.get_zone_instance_version()) != eq.get_rule("Custom:StaticInstanceVersion")) then -- Only spawn Badain in non-respawning dz
 		e.self:Depop();
 	end
 	war_started = false
