ca4b33fb3a5dbbe4b1a7ddb4bfa651ef6175db65
RetributionEQ <info@retributioneq.com>
2025-02-02 14:58:12 -0500

    Move echoes into their zones

--- velketor/Echo_of_the_Past.pl
+++ velketor/Echo_of_the_Past.pl
@@ -1,7 +1,7 @@
-my $expedition_name = "The Great Divide";
+my $expedition_name = "Velketor's Labyrinth";
 my $min_players     = 1;
 my $max_players     = 72;
-my $dz_zone         = "greatdivide";
+my $dz_zone         = "velketor";
 
 sub EVENT_SAY {
   plugin::OfferStandardInstance($expedition_name, $min_players, $max_players, $dz_zone);
